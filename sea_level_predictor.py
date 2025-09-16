764c86502274a2d08e6d454c1762265b198415b5
Ana Cristina Alves Jardim <ana.jardim@sou.inteli.edu.br>
2025-09-15 22:10:01 -0300

    feat: first line of regression

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,14 +4,22 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level']  )
 
-
+    #https://www.w3schools.com/python/python_ml_linear_regression.asp
     # Create first line of best fit
+    slope, intercept, r, p, std_err = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
 
+    def lineCalculation(x):
+        return slope * x + intercept
+    
+    y = list(map(lineCalculation, df['Year']))
 
+    plt.plot(df['Year'], y)
+    plt.show()
     # Create second line of best fit
 
 
